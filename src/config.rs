703c34c58871f94f0e46a5789fa49f4d4092bda3
Brage Fuglseth <brage.fuglseth@gmail.com>
2023-07-01 15:37:04 +0200

    translations: update potfile

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,5 @@
-pub static APP_ID: &str = "dev.bragefuglseth.Fretboard.Devel";
-pub static VERSION: &str = "1.0-7ec6637";
+pub static APP_ID: &str = "dev.bragefuglseth.Fretboard";
+pub static VERSION: &str = "1.0";
 pub static GETTEXT_PACKAGE: &str = "fretboard";
-pub static LOCALEDIR: &str = "/app/share/locale";
-pub static PKGDATADIR: &str = "/app/share/fretboard";
+pub static LOCALEDIR: &str = "/usr/local/share/locale";
+pub static PKGDATADIR: &str = "/usr/local/share/fretboard";
