7ec663708a78b7a0fced759c89176256e9bf30f8
Brage Fuglseth <brage.fuglseth@gmail.com>
2023-07-01 14:06:08 +0200

    metadata: add app icon

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
 pub static VERSION: &str = "1.0";
 pub static GETTEXT_PACKAGE: &str = "fretboard";
-pub static LOCALEDIR: &str = "/usr/local/share/locale";
-pub static PKGDATADIR: &str = "/usr/local/share/fretboard";
+pub static LOCALEDIR: &str = "/app/share/locale";
+pub static PKGDATADIR: &str = "/app/share/fretboard";
