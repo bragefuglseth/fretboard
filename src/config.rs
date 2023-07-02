a105dff3d9519b358e135b353ceadf00739af0b8
Brage Fuglseth <brage.fuglseth@gmail.com>
2023-07-03 01:09:22 +0200

    config: remove config.rs

--- src/config.rs
+++
@@ -1,5 +0,0 @@
-pub static APP_ID: &str = "dev.bragefuglseth.Fretboard";
-pub static VERSION: &str = "1.0";
-pub static GETTEXT_PACKAGE: &str = "fretboard";
-pub static LOCALEDIR: &str = "/usr/local/share/locale";
-pub static PKGDATADIR: &str = "/usr/local/share/fretboard";
